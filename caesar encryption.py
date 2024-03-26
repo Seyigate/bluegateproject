fe766d0991a41f6a24123cc669a54762b4eae1c4
Seyigate <156173098+Seyigate@users.noreply.github.com>
2024-03-26 03:37:14 +0000

    Update bluegate_cipher.py

--- caesar encryption.py
+++ caesar encryption.py
@@ -1,5 +1,3 @@
-#https://ascii.co.uk/text
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
@@ -16,11 +14,9 @@ def bluegate(start_text, shift_amount, cipher_direction):
             end_text += alphabet[new_position]
         else:
             end_text += letter
+                    
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
-
-# TODO-1: Import and print the logo from art.py when the program starts.
-#from art import logo
 logo = """                                                                                                                                  
 88888888ba  88                          ,ad8888ba,                                    ,ad8888ba,  88             88                                 
 88      "8b 88                         d8"'    `"8b              ,d                  d8"'    `"8b ""             88                                 
@@ -36,20 +32,13 @@ logo = """
 
 print(logo)
 
-# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
 should_end = False
 while not should_end:
 
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    # TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-    # Try running the program and entering a shift number of 45.
-    # Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-    # Hint: Think about how you can use the modulus (%).
+    
     shift = shift % 26
 
     bluegate(start_text=text, shift_amount=shift, cipher_direction=direction)
@@ -58,6 +47,9 @@ while not should_end:
     if restart == "no":
         should_end = True
         print("Goodbye")
+
+#For any feedback or comment kindly contact bluegate.log@gmail.com
+#Thank you for your time and support
 
 
 
